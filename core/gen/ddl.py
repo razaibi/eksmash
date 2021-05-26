c89cb1cd21bd2275678f04a7dcad73042534f285
Raza Balbale <ribalbale@gmail.com>
2021-05-26 03:08:12 +0000

    Added files for DDL generators.

--- core/gen/ddl.py
+++ core/gen/ddl.py
@@ -1,10 +1,13 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 import os
 import CONFIGS
 import core.common
 
 =======
 >>>>>>> 2c59c584b84cd7a93781a1a2385b72244e6f4a0a
+=======
+>>>>>>> origin/main
 def create_ddl(action, file):
     action_swticher = {
         "create" : create_new_database,
